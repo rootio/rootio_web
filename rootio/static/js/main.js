f5e1d183b5bf80ed7b05e5c4eaf70db83a4ef9fd
Josh Levinger <josh.l@engagementlab.org>
2013-10-06 22:54:44 -0700

    cleanup cruft

--- rootio/static/js/main.js
+++ rootio/static/js/main.js
@@ -1,12 +1,3 @@
-/* Author: Wilson Xu */
-
-function hide_flask_message_container() {
-    $('#flash_message_container').slideUp('fast');
-}
-
 $(document).ready(function() {
-    /* Show and hide flash message. */
-    //$('#flash_message_container').slideDown(function() {
-        //setTimeout(hide_flask_message_container, 3000);
-    //});
+    //sitewide customizations here    
 })
