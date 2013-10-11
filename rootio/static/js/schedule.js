9306ae0374465711a21fd4dac4cf67116323691f
Josh Levinger <josh.l@engagementlab.org>
2013-10-10 21:21:00 -0700

    schedule uses blocks

--- rootio/static/js/schedule.js
+++ rootio/static/js/schedule.js
@@ -1,42 +1,4 @@
 $(document).ready(function() {    
-    $('#calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-        },
-        allDayDefault: false,
-        defaultView: 'agendaWeek',
-        editable: false,
-        events: {
-            url: '/radio/station/schedule.json'
-        }
-    });
 
-    //set up presets
-    $('input[name=preset-recurrence]').change(function(event) {
-        var value = $(event.target).val();
-        if (value == "custom") {
-            $('#recurringinput').show();
-        } else {
-            $('#recurringinput').hide();
-            $('form input[name=recurrence]').val(value);
-        }
-    });
-
-    //init recurring input fieldset for custom schedules
-    $('#recurringinput').recurringinput().hide();
-    //and watch for updates
-    $('#recurringinput').on('rrule-update',function(event) {
-        var rrule = $('#recurringinput #rrule-output').html();
-        $('form input[name=recurrence]').val(rrule);
-    });  
-
-
-    //shim html5 input types
-    if (!Modernizr.inputtypes.date) {
-        $('input[type=date]').datepicker();
-    }
-    
 });
 
