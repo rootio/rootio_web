f0ad2ee51ad0e6589e8da881255a6a6e7543b7e8
Josh Levinger <josh@levinger.net>
2013-10-25 18:30:57 +0300

    stamp initial revision

--- alembic/versions/dc611df9442_make_telephony.py
+++ alembic/versions/dc611df9442_make_telephony.py
@@ -1,13 +1,13 @@
 """make telephony_phonenumber raw_number nullable True
 
-Revision ID: dc611df9442
+Revision ID: initial
 Revises: 57f461d001ec
 Create Date: 2013-10-25 18:15:47.156128
 
 """
 
 # revision identifiers, used by Alembic.
-revision = 'dc611df9442'
+revision = 'initial'
 down_revision = '57f461d001ec'
 
 from alembic import op
