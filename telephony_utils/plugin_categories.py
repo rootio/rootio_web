ee583d517111ea5196a7d613c5ee348ded379fe7
Josh Levinger <josh@levinger.net>
2013-10-25 13:02:42 +0300

    moved telephony_utils to new repo rootio_telephony

--- telephony_utils/plugin_categories.py
+++
@@ -1,5 +0,0 @@
-from yapsy.IPlugin import IPlugin
-
-class IProgram(IPlugin):
-    def print_name(self):
-        print "This is a blank program"
--- telephony_utils/plugins/plugin1.py
+++
@@ -1,6 +0,0 @@
-from yapsy.IPlugin import IPlugin
-
-class PluginOne(IPlugin):
-    def print_name(self):
-        print "This is plugin 1"
-
