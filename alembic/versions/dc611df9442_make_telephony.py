83644200abda83cdaf3c10dde1093a3c906b9d24
Josh Levinger <josh@levinger.net>
2013-10-25 18:34:13 +0300

    fix parent

--- alembic/versions/dc611df9442_make_telephony.py
+++ alembic/versions/dc611df9442_make_telephony.py
@@ -1,14 +1,14 @@
 """make telephony_phonenumber raw_number nullable True
 
-Revision ID: initial
-Revises: 57f461d001ec
+Revision ID: dc611df9442
+Revises: initial
 Create Date: 2013-10-25 18:15:47.156128
 
 """
 
 # revision identifiers, used by Alembic.
-revision = 'initial'
-down_revision = '57f461d001ec'
+revision = 'dc611df9442'
+down_revision = 'initial'
 
 from alembic import op
 import sqlalchemy as sa
