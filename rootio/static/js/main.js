1ea99b8768ef97d6394401ec18723d6197347e30
Josh Levinger <josh@levinger.net>
2014-03-07 14:06:08 -0800

    minor js fixes

--- rootio/static/js/main.js
+++ rootio/static/js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    var csrftoken = $('meta[name=csrf-token]').attr('content')
+    var csrftoken = $('meta[name=csrf-token]').attr('content');
     $.ajaxSetup({
         beforeSend: function(xhr, settings) {
             if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken)
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
             }
         }
-    })
+    });
     //sitewide customizations here
 
     $('form#language-selector select').on('change',function() {
-        console.log('change!');
+        console.log('language changed');
         this.form.submit();
     });
-})
+});
