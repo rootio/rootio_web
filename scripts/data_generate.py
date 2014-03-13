bcb435bcfd9cc4500729158bd3bc03d0a3380920
Josh Levinger <josh@levinger.net>
2014-03-13 10:52:08 -0700

    link locations to dummy stations in data_generate

--- scripts/data_generate.py
+++ scripts/data_generate.py
@@ -79,34 +79,33 @@ db.session.commit()
 #l.addressline1  l.country       l.id            l.longitude     l.modifieddate  l.name          l.query_class   
 #l.addressline2  l.district      l.latitude      l.metadata      l.municipality  l.query   
 
-l = Location(latitude=.12, longitude=30.5, country='Uganda',district='Ibanda',name='Ibanda',municipality='Ibanda')
-db.session.add(l)
+l0 = Location(latitude=.12, longitude=30.5, country='Uganda',district='Ibanda',name='Ibanda',municipality='Ibanda')
+db.session.add(l0)
 db.session.commit()
 
-l = Location(latitude=3.712811,longitude=31.783345, country='Uganda',district='Metuli',name='Metuli',municipality='Metuli')
-db.session.add(l)
+l1 = Location(latitude=3.712811,longitude=31.783345, country='Uganda',district='Metuli',name='Metuli',municipality='Metuli')
+db.session.add(l1)
 db.session.commit()
 
-l = Location(latitude=2.695784,longitude=32.029277, country='Uganda',district='',name='Alero',municipality='Alero')
-db.session.add(l)
+l2 = Location(latitude=2.695784,longitude=32.029277, country='Uganda',district='',name='Alero',municipality='Alero')
+db.session.add(l2)
 db.session.commit()
 
-
-l = Location(latitude=1.161839,longitude=32.815294, country='Uganda',district='',name='Mugongo',municipality='Mugongo')
-db.session.add(l)
+l3 = Location(latitude=1.161839,longitude=32.815294, country='Uganda',district='',name='Mugongo',municipality='Mugongo')
+db.session.add(l3)
 db.session.commit()
 
-
-l = Location(latitude=2.316237,longitude=32.686708, country='Uganda',district='',name='Aber',municipality='Aber')
-db.session.add(l)
+l4 = Location(latitude=2.316237,longitude=32.686708, country='Uganda',district='',name='Aber',municipality='Aber')
+db.session.add(l4)
 db.session.commit()
 
-
-l = Location(latitude=2.634716,longitude=31.997509, country='Uganda',district='Gulu',name='Tam Pi Diki',municipality='Tam Pi Diki')
-db.session.add(l)
+l5 = Location(latitude=2.634716,longitude=31.997509, country='Uganda',district='Gulu',name='Tam Pi Diki',municipality='Tam Pi Diki')
+db.session.add(l5)
 db.session.commit()
 
-
+l6 = Location(latitude=3.0016701,longitude=32.1443653, country='Uganda',district='',name='Pabo',municipality='Pabo')
+db.session.add(l6)
+db.session.commit()
 
 #2.695784, 32.029277 Alero
 #1.161839, 32.815294 Mugongo
@@ -131,13 +130,13 @@ def id_generator(size=10, chars=string.ascii_letters + string.digits):
     #return base64.urlsafe_b64encode(os.urandom(size))
     return ''.join(random.choice(chars) for x in range(size))
 
-s0 = Station(name='Radio Aber', frequency=88.5, api_key=id_generator())
-s1 = Station(name='Pabo FM', frequency=89.0, api_key=id_generator())
-s2 = Station(name='Ibanda FM', frequency=102.6, api_key=id_generator())
-s3 = Station(name='Metuli RootIO', frequency=102.6, api_key=id_generator())
-s4 = Station(name='Alero Community Radio', frequency=102.6, api_key=id_generator())
-s5 = Station(name='Mugongo Farm Fresh SACCO', frequency=102.6, api_key=id_generator())
-s6 = Station(name='Tom Pi Diki FM', frequency=95.5, api_key=id_generator())
+s0 = Station(name='Ibanda FM', location=l0, frequency=102.6, api_key=id_generator())
+s1 = Station(name='Metuli RootIO', location=l1, frequency=102.6, api_key=id_generator())
+s2 = Station(name='Alero Community Radio', location=l2, frequency=102.6, api_key=id_generator())
+s3 = Station(name='Mugongo Farm Fresh SACCO', location=l3, frequency=102.6, api_key=id_generator())
+s4 = Station(name='Radio Aber', location=l4, frequency=88.5, api_key=id_generator())
+s5 = Station(name='Tam Pi Diki FM', location=l5, frequency=95.5, api_key=id_generator())
+s6 = Station(name='Pabo FM', location=l6, frequency=89.0, api_key=id_generator())
 db.session.add(s0)
 db.session.add(s1)
 db.session.add(s2)
