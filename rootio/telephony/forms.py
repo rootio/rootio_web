0e02958d3cb7fa60e6edaee5d8589a56de841705
Jude Mukundane <jude19love@gmail.com>
2014-04-27 18:14:51 -0700

    Added gateway form.

--- rootio/telephony/forms.py
+++ rootio/telephony/forms.py
@@ -1,11 +1,12 @@
 from flask.ext.wtf import Form
 from wtforms.ext.sqlalchemy.orm import model_form
-from wtforms import SubmitField, RadioField
+from wtforms import SubmitField, RadioField, IntegerField, StringField
 from wtforms.validators import AnyOf
+from sqlalchemy import Column, Table, types
 
 from .constants import PHONE_NUMBER_TYPE
 
-from .models import PhoneNumber
+from .models import PhoneNumber, Gateway
 from ..extensions import db
 
 PhoneNumberFormBase = model_form(PhoneNumber, db_session=db.session, base_class=Form,
@@ -13,4 +14,13 @@ PhoneNumberFormBase = model_form(PhoneNumber, db_session=db.session, base_class=
 class PhoneNumberForm(PhoneNumberFormBase):
     number_type = RadioField(u"Type", [AnyOf([str(val) for val in PHONE_NUMBER_TYPE.keys()])],
             choices=[(str(val), label) for val, label in PHONE_NUMBER_TYPE.items()])
+    submit = SubmitField(u'Save')
+
+GatewayFormBase = model_form(Gateway, db_session=db.session, base_class=Form, exclude=['created_at','updated_at'])
+class GatewayForm(GatewayFormBase):
+    name = StringField()
+    number_top = db.Column(db.Integer)
+    number_bottom = db.Column(db.Integer)
+    sofia_string = StringField()
+    extra_string = StringField()
     submit = SubmitField(u'Save')
