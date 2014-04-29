f094cc2e87d121111319b50fee086858682e57e6
Josh Levinger <josh@levinger.net>
2014-04-28 19:09:47 -0700

    interactive scheduler message tester

---
+++ rootio/scheduler/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, request, current_app, Response
+from ..decorators import returns_json
+
+import json
+
+scheduler = Blueprint('scheduler', __name__, url_prefix='/scheduler')
+
+@scheduler.route('/message_test/', methods=['GET'])
+def message_test():
+    return render_template('scheduler/message_test.html')
+
+@scheduler.route('/message_test_ajax/', methods=['POST'])
+@returns_json
+def message_test_ajax():
+    data_list = json.loads(request.data)
+    data = {}
+    for d in data_list:
+        data[d['name']] = d['value']
+    #decode form...
+
+    try:
+        msg = data.get('message').__str__()
+        topic = data.get('topic').__str__()
+        current_app.messenger.send_multipart([b"%s" % topic, b"%s" % msg])
+        return {'status_code':200,'topic':topic,'message':msg}
+    except Exception,e:
+        return {'status_code':400, 'errors':str(e)}
