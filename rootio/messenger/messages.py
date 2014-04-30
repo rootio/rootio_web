5ecbf4c029e17633ed0e263ee42f46a600d30126
Josh Levinger <josh@levinger.net>
2014-04-29 22:54:52 -0700

    cleanup, add station_update_fields message

--- rootio/messenger/messages.py
+++ rootio/messenger/messages.py
@@ -6,10 +6,22 @@ def test_message(topic, msg):
     return (topic, msg)
 
 @sends_json
-def schedule_program_start(program_id, station_id, start_time):
-    #tells the scheduler to start program_id on station_id at start_time
+def schedule_program(operation, program_id, station_id, start_time, updated=False):
+    """ Tells the scheduler to start program_id on station_id at start_time
+     If operation is 'delete', scheduler will remove existing job
+     If operation is 'update', scheduler will remove and re-add job
+    """
     topic = "scheduler"
-    msg = {'program_id': program_id,
+    msg = {'operation': operation,
+           'program_id': program_id,
            'station_id': station_id,
            'start_time': start_time}
+    return (topic, msg)
+
+@sends_json
+def station_update_fields(station_id, fields):
+    """ Tells the telephony_server to update the client about new station fields """
+    topic = "station.%d" % station_id
+    msg = {'operation': 'update',
+           'fields': fields}
     return (topic, msg)
