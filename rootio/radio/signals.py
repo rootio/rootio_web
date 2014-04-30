f9d777a40c842e1ab60b89c3561d04dbdea5b653
Josh Levinger <josh@levinger.net>
2014-04-29 22:00:09 -0700

    cleanup

--- rootio/radio/signals.py
+++ rootio/radio/signals.py
@@ -6,11 +6,11 @@ from ..messenger import messages
 @models_committed.connect
 def on_models_committed(sender, changes):
     for obj, change in changes:
-        print (obj, change)
         if isinstance(obj, ScheduledProgram):
             # can't do any foreignkey lookups here b/c db is in commit, just id fields
             # have to load anything else necessary when it hits telephony_server
             messages.schedule_program_start(obj.program_id, obj.station_id, obj.start)
 
         #other models we need to send messages about?
-            #station whitelisted numbers
+            # station db fields update (gateway, client_update_frequency, etc)
+            # station whitelisted numbers
