5e3c18fd35d50e055a234c99e837bd4fb4741b07
Josh Levinger <josh@levinger.net>
2014-04-30 19:10:54 -0700

    use logging instead of print

--- scheduler/run.py
+++ scheduler/run.py
@@ -1,9 +1,12 @@
 import atexit
+import logging
+import argparse
+
 from env import read_env
 from broker import MessageBroker
 from scheduler import MessageScheduler
 
-if __name__ == "__main__":
+def run():
     config = read_env('config.cfg')
     
     scheduler = MessageScheduler(config['jobstore'],config['url'])
@@ -20,3 +23,19 @@ if __name__ == "__main__":
         broker.start()
     except KeyboardInterrupt:
         broker.shutdown()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='RootIO Scheduled Message Broker')
+
+    parser.add_argument('--log', action='store', help='log level', default='debug')
+    args = parser.parse_args()
+
+    numeric_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.log)
+    logging.basicConfig(name='rootio_scheduler',level=numeric_level)
+
+    try:
+        run()
+    finally:
+        logging.shutdown()
--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,4 +1,5 @@
 from apscheduler.scheduler import Scheduler
+import logging
 
 class MessageScheduler(object):
     def __init__(self, jobstore, url):
@@ -9,23 +10,23 @@ class MessageScheduler(object):
         self._scheduler.configure(config)
 
     def start(self):
-        print "scheduler start"
+        logging.info("scheduler start")
         self._scheduler.start()
     def shutdown(self):
-        print "scheduler shutdown"
+        logging.info("scheduler shutdown")
         self._scheduler.shutdown()
 
     def schedule_message(self, topic, message, send_at):
-        print "schedule message %s:%s at %s" % (topic, message, send_at)
+        logging.info("schedule message %s:%s at %s" % (topic, message, send_at))
 
         #create lambda for scheduler to call at execution time
 
         #and add it
         job = self._scheduler.add_date_job(self._broker.forward, send_at, args=(topic, message))
-        print "scheduled job_id", job.id
+        logging.debug("scheduled job_id", job.id)
 
     def cancel_message(self, message_id):
-        print "cancel message_id %s" % message_id
+        logging.info("cancel message_id %s" % message_id)
         # apscheduler.unschedule_job works by comparing apscheduler.job objects
         # we don't have a whole job, just the id, message_id == job.id
         # so do it manually
@@ -43,6 +44,6 @@ class MessageScheduler(object):
         raise KeyError('Message id "%s" is not scheduled in any job store' % message_id)
 
     def reschedule_message(self, message_id, topic, message, send_at):
-        print "reschedule message_id %s" % message_id
+        logging.info("reschedule message_id %s" % message_id)
         self.cancel_message(message_id)
         self.schedule_message(topic, message, send_at)
