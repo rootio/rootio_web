b5694a4b55a28e455081c068133cf5e7a6dc87b8
Csik <root@server1.rootio.org>
2014-06-19 09:04:32 +0000

    Removed unused broker, added forwarder testers to test_scripts

---
+++ scheduler/test_scripts/forwarder_server.py
@@ -0,0 +1,17 @@
+import zmq
+import random
+import sys
+import time
+
+port = "55777"
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.connect("tcp://localhost:%s" % port)
+publisher_id = random.randrange(0,9999)
+while True:
+    topic = random.randrange(1,10)
+    messagedata = "server#%s" % publisher_id
+    print "%s %s" % (topic, messagedata)
+    socket.send("%d %s" % (topic, messagedata))
+    time.sleep(1)
+
