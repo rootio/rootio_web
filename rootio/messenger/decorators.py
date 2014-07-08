31653354bc65bb16552a5d9f4bf9ad17b65e808b
Jude Mukundane <jude19love@gmail.com>
2014-07-07 22:00:53 -0700

    Added obj_id awareness to scheduler, saving a name along with callback function so that jobs can be found later.  PEP8 all round."

--- rootio/messenger/decorators.py
+++ rootio/messenger/decorators.py
@@ -4,12 +4,13 @@ from flask import current_app
 from ..utils import CustomJSONEncoder
 import json
 
+
 def sends_multipart(f):
     """Takes a tuple (topic, message) and sends it to the scheduler,
     which will pass it at the appointed time to telephony_server"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         topic, msg = f(*args, **kwargs)
-	msg = json.dumps(msg, cls=CustomJSONEncoder)
+        msg = json.dumps(msg, cls=CustomJSONEncoder)
         current_app.messenger.send_multipart((topic, msg))
     return decorated_function
