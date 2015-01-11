eb48abc88766317803bf6c352df55e671f77ee02
chris csikszentmihalyi <robotic@gmail.com>
2015-01-11 21:29:43 +0000

    version running on server

--- scheduler/test_scripts/scheduler_tester.py
+++ scheduler/test_scripts/scheduler_tester.py
@@ -15,7 +15,7 @@ socket.connect("tcp://localhost:%s" % port)
 topic = 'scheduler'
 start = datetime.datetime.now(tzutc()).replace(microsecond=0) + datetime.timedelta(seconds = 15)
 start = datetime.datetime.isoformat(start)
-msg = """{"operation": "update", "start_time": "%s", "station_id": 8, "program_id": 5, "obj_id": 451}""" % start
+msg = """{"operation": "update", "start_time": "%s", "station_id": 5, "program_id": 5, "obj_id": 451}""" % start
 multipart = [topic, msg]
 time.sleep(.1)
 print multipart
