cdc7a1bcbf34aea1a70f0935747451d86deabf36
fcl <fabiocl93@gmail.com>
2016-09-10 19:34:27 +0100

    #Sparkline lib update # Change on sparkline colors # Semi-Resolution on tooltip for line sparklines # Nav Bar minimized when screen is small # Colors change when mouse over sidebar

--- rootio/static/js/sidebar.js
+++ rootio/static/js/sidebar.js
@@ -1,40 +1,53 @@
 /**
  * Created by fcl on 30-08-2016.
  */
+
+/*Handle th nav bar operations close and open*/
 $(".toggle-nav").click(function () {
     if ( $(".side-nav").css("width") == "250px") {
-        console.log("Close Bar" + $(".side-nav").css("width"))
+        /*console.log("Close Bar" + $(".side-nav").css("width"))*/
         closeSideBar()
     }
     else{
-        console.log("Open Bar"+ $(".side-nav").css("width"))
+        /*console.log("Open Bar"+ $(".side-nav").css("width"))*/
         openSideBar()
 
     }
 })
 
+$(".side-nav .dropdown").click(function () {
+    if($(".side-nav").css("width") == "50px") {
+        openSideBar()
+    }
+})
 
+/*APPLY CSS to close the nav bar*/
 function closeSideBar(){
+    $(".side-nav .navside-tabs a span, .side-nav .navside-tabs a .fa-chevron-down").css({
+        display:"none"
+    })
     $(".side-nav").css({
-        width: "0px"
+        width: "50px"
     })
     $(".content-container").css({
-       marginLeft: "0px"
+       marginLeft: "50px"
     })
     $(".header-content-header").css({
-       marginLeft: "0px"
+       marginLeft: "50px"
     })
     $(".button-form-row").css({
-       marginLeft: "0px"
+       marginLeft: "50px"
     })
-
-
     closed = true
 }
 
+/*APPLY css to open the nav bar*/
 function openSideBar(){
     $(".side-nav").css({
         width: "250px"
+    })
+    $(".side-nav .navside-tabs a span, .side-nav .navside-tabs a .fa-chevron-down").css({
+        display: "inline"
     })
     $(".content-container").css({
         marginLeft: "250px"
@@ -47,3 +60,28 @@ function openSideBar(){
     })
     closed = false;
 }
+
+/*TODO - MAYBE LATER USE SASS OR LESS TO DO THIS*/
+/*Hover events CSS*/
+
+$(".side-nav .dropdown,.side-nav .no-dropdown").mouseover(function(){
+    $(this).children("a").children("i").css({
+        color: "#009688"
+    })
+}).mouseout(function () {
+    $(this).children("a").children("i").css({
+        color: "#3c3c3c"
+    })
+})
+
+
+
+$(".side-nav .dropdown-menu li a").mouseover(function () {
+    $(this).children("i").css({
+        color: "#009688"
+    })
+}).mouseout(function () {
+    $(this).children("i").css({
+        color: "#3c3c3c"
+    })
+})
