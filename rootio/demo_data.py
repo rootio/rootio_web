429432ed250116657a0dcfd3677a8f96e8738d36
Alex Morega <alex@grep.ro>
2016-12-07 15:19:17 +0200

    update `demo_data.py` to work with new code

--- rootio/demo_data.py
+++ rootio/demo_data.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 from datetime import datetime, timedelta
 import json
-import flask
 import pytz
 from .user.models import User
 from .radio.models import Network, Station, Program, ScheduledProgram
 from .telephony.models import PhoneNumber, Gateway
 from .telephony.constants import MOBILE
+from .content.models import ContentTrack, ContentUploads
 
-app = flask.current_app
-MEDIA_PREFIX = Path(app.config['MEDIA_PREFIX'])
+from .config import DefaultConfig
+DEMO_MEDIA_PREFIX = Path(DefaultConfig.DEMO_MEDIA_PREFIX)
 
 PHONE_NUMBER = '1007'
 GATEWAY_NAME = 'Internal 4000'
@@ -17,17 +17,6 @@ NETWORK_NAME = "Testy network"
 STATION_NAME = "Testy sounds"
 PROGRAM_NAME = "BBC Africa Today"
 PROGRAM_DURATION = 40
-PROGRAM_DESCRIPTION = {
-    'Media': [
-        {
-            'argument': [str(MEDIA_PREFIX / 'bbc/latest_africa.mp3')],
-            'start_time': '00:00:01',
-            'duration': PROGRAM_DURATION,
-            'is_streamed': True,
-            'hangup_on_complete': True,
-        },
-    ],
-}
 
 def setup(db, schedule):
     admin = User.query.get(1)
@@ -54,11 +43,13 @@ def setup(db, schedule):
             is_goip=False,
         )
         db.session.add(gateway)
+        db.session.flush()
 
     network = Network.query.filter_by(name=NETWORK_NAME).first()
     if network is None:
         network = Network(
             name=NETWORK_NAME,
+            #networkusers=[admin],
         )
 
     station = Station.query.filter_by(name=STATION_NAME).first()
@@ -79,25 +70,53 @@ def setup(db, schedule):
         db.session.add(station)
         db.session.flush()
 
+    TRACK_NAME = 'testdata-bbc-track'
+    track = ContentTrack.query.filter_by(name=TRACK_NAME)
+    if track is None:
+        track = ContentTrack(
+            name=TRACK_NAME,
+        )
+        db.session.add(track)
+        db.session.flush()
+
+    UPLOAD_NAME = 'testdata-bbc-upload'
+    upload = ContentUploads.query.filter_by(name=UPLOAD_NAME).first()
+    if upload is None:
+        upload = ContentUploads(
+            name=UPLOAD_NAME,
+            order=1,
+        )
+        db.session.add(upload)
+        db.session.flush()
+
     program = Program.query.filter_by(name=PROGRAM_NAME).first()
     if program is None:
+        media_action = {
+            'type': 'Media',
+            'track_id': upload.track_id,
+            'start_time': '00:00:01',
+            'duration': PROGRAM_DURATION,
+        }
+        structure = [media_action]
+
         program = Program(
             name=PROGRAM_NAME,
-            description=json.dumps(PROGRAM_DESCRIPTION, indent=2),
             duration=timedelta(minutes=PROGRAM_DURATION),
+            structure=json.dumps(structure, indent=2),
         )
         db.session.add(program)
         db.session.flush()
 
     if schedule:
-        station.scheduled_programs.delete()
         now = pytz.utc.localize(datetime.utcnow())
         start = now + timedelta(seconds=schedule)
+
         scheduled_program = ScheduledProgram(
-            station=station,
+            station_id=station.id,
             program=program,
             start=start,
             end=start + timedelta(minutes=PROGRAM_DURATION),
+            deleted=False,
         )
         db.session.add(scheduled_program)
         db.session.flush()
