22b72dff6e5a59c452b0625481210448001fe194
Alex Morega <alex@grep.ro>
2018-04-16 15:25:08 +0000

    fix login check for "user is active"

--- rootio/user/models.py
+++ rootio/user/models.py
@@ -7,7 +7,7 @@ from flask.ext.login import UserMixin
 
 from ..extensions import db
 from ..utils import get_current_time, GENDER_TYPE, STRING_LEN
-from .constants import NETWORK_USER, USER_ROLE, ADMIN, INACTIVE, USER_STATUS
+from .constants import NETWORK_USER, USER_ROLE, ADMIN, INACTIVE, ACTIVE, USER_STATUS
 
 class UserDetail(db.Model):
     __tablename__ = 'user_details'
@@ -92,7 +92,7 @@ class User(RootioUser, UserMixin):
 
         if user:
             authenticated = user.check_password(password)
-            activated = user.status_code == 1
+            activated = (user.status_code == ACTIVE)
         else:
             activated = False
             authenticated = False
