97abaf45ed55ea2248bdda4ec6d1a4d091d57e6c
Alex Morega <alex@grep.ro>
2018-04-18 12:39:35 +0000

    create ContentType rows

--- rootio/demo_data.py
+++ rootio/demo_data.py
@@ -4,6 +4,7 @@ import json
 import pytz
 from .user.models import User, RootioUser
 from .radio.models import Location, Network, Station, Program, ScheduledProgram
+from .radio.models import ContentType
 from .telephony.models import PhoneNumber, Gateway
 from .telephony.constants import MOBILE
 from .content.models import ContentTrack, ContentUploads
@@ -146,5 +147,24 @@ def setup(db, schedule):
         )
         db.session.add(scheduled_program)
         db.session.flush()
+
+    content_type_media = ContentType.query.filter_by(name='Media').first()
+    if content_type_media is None:
+        content_type_media = ContentType(
+            name='Media',
+            description='',
+        )
+        db.session.add(content_type_media)
+        db.session.flush()
+
+    content_type_news = ContentType.query.filter_by(name='News').first()
+    if content_type_news is None:
+        content_type_news = ContentType(
+            name='News',
+            description='',
+        )
+        db.session.add(content_type_news)
+        db.session.flush()
+    print content_type_news
 
     db.session.commit()
