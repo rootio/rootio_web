192fb74061050174245b424fff0ba4bcd155ae1b
Jude Mukundane <jude19love@gmail.com>
2018-10-10 12:49:00 +0100

    Stabilization to the podcast download runner, including DB connection mgt

--- telephony/podcast/rss_agent.py
+++ telephony/podcast/rss_agent.py
@@ -14,9 +14,19 @@ class RSSAgent:
         self.__logger = logger
 
     def __get_podcast_tracks(self):
-        engine = create_engine(DefaultConfig.SQLALCHEMY_DATABASE_URI)
-        session = sessionmaker(bind=engine)()
-        return session.query(ContentPodcast).all()
+        session = None
+        try:
+            engine = create_engine(DefaultConfig.SQLALCHEMY_DATABASE_URI)
+            session = sessionmaker(bind=engine)()
+            return session.query(ContentPodcast).all()
+        except:
+            return []
+        finally:
+            try:
+                if session is not None:
+                    session.close()
+            except: # some other error
+                pass  # log this
 
     def run(self):
         while True:
