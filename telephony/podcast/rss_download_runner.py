461c9ab076d6ce9173b1b30c44cb5bb7f71521ee
Jude Mukundane <jude19love@gmail.com>
2018-10-24 17:44:05 +0100

    Rectified relation between program and calls.

--- telephony/podcast/rss_download_runner.py
+++ telephony/podcast/rss_download_runner.py
@@ -6,16 +6,15 @@ __author__="HP Envy"
 __date__ ="$Nov 19, 2014 4:16:15 PM$"
 
 import sys
-import os
+
 sys.path.append('/usr/local/rootio_web/')
 from rootio.config import *
 from telephony.daemon.daemoner import Daemon
-from rootio.radio.models import Station
 from rss_agent import RSSAgent
 from datetime import datetime
-import threading
 import logging
 from logging.handlers import TimedRotatingFileHandler 
+
 
 class RSSRunner(Daemon):
 
@@ -33,10 +32,9 @@ class RSSRunner(Daemon):
         print "================ RSS runner service started at {0} ==============".format(datetime.utcnow())
 
 
-
 if __name__ == "__main__":
     rss_daemon = RSSRunner("/tmp/rss_runner.pid")
-    if(len(sys.argv) == 2):
+    if len(sys.argv) == 2:
         if sys.argv[1] == "start":
             rss_daemon.start()
         elif sys.argv[1] == "stop":
