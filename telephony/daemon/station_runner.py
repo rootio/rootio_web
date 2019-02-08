3f6ca0a7a29a848c04d002fad3574d344795c02e
Jude Mukundane <jude19love@gmail.com>
2019-02-08 16:22:32 +0000

    Added cataloging for music files by station runner service

--- telephony/daemon/station_runner.py
+++ telephony/daemon/station_runner.py
@@ -85,7 +85,7 @@ class StationRunner():
             thrd.start()
 
     def __handle_tcp_connection(self, sck):  # TODO: handle json errors, else server will break due to rogue connection
-        data = sck.recv(1024)
+        data = sck.recv(102400)
         try:
             event = json.loads(data)
         except ValueError:
