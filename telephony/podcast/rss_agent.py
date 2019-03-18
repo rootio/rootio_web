a151fdd43f695649f3f0b332d08b21f4f1663543
Jude Mukundane <jude19love@gmail.com>
2019-03-18 21:06:58 +0000

    Even more logging in the podcast downloader to ease debugging

--- telephony/podcast/rss_agent.py
+++ telephony/podcast/rss_agent.py
@@ -19,24 +19,27 @@ class RSSAgent:
             engine = create_engine(DefaultConfig.SQLALCHEMY_DATABASE_URI)
             session = sessionmaker(bind=engine)()
             return session.query(ContentPodcast).all()
-        except:
+        except Exception as e:
+            self.__logger.error("error in __get_podcast_tracks: {0}".format(e.message))
             return []
         finally:
             try:
                 if session is not None:
                     session.close()
-            except: # some other error
+            except Exception as e:  # some other error:
+                self.__logger.error("error in __get_podcast_tracks(finally): {0}".format(e.message))
                 pass  # log this
 
     def run(self):
         while True:
             try:
-                self.__logger.info("Checking for new podcasts")
-                for podcast_track in self.__get_podcast_tracks():
+                podcast_tracks = self.__get_podcast_tracks()
+                self.__logger.info("Checking for new podcasts in: {0}".format(podcast_tracks))
+                for podcast_track in podcast_tracks:
                     pd = RSSDownloader(podcast_track.id, self.__logger)
                     thr = threading.Thread(target=pd.download)
                     thr.daemon = True
                     thr.start()
                 sleep(300)  # 5 minutes
             except Exception as e:
-                self.__logger.error(e.message)
+                self.__logger.error("error in run: {0}".format(e.message))
