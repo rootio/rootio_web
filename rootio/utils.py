24fbfba961c0ae05bc1198dd87ad676789e6d284
Jude Mukundane <jude19love@gmail.com>
2019-03-28 18:08:50 +0000

    Basic audio processing of uploaded audio files

--- rootio/utils.py
+++ rootio/utils.py
@@ -11,6 +11,7 @@ import urllib
 from datetime import datetime, time, timedelta
 
 import boto3
+import sox
 from flask import json
 from flask.ext.wtf import Form
 from sqlalchemy import or_
@@ -75,7 +76,7 @@ def upload_to_s3(file, key,  acl="public-read"):
     return uri
 
 
-def save_uploaded_file(uploaded_file, directory, file_name=False):
+def save_uploaded_file(uploaded_file, directory, file_name=False, process_audio=True):
     date_part = datetime.now().strftime("%y%m%d%H%M%S")
     if not file_name:
         file_name = "{0}_{1}".format(date_part, uploaded_file.filename)
@@ -98,6 +99,10 @@ def save_uploaded_file(uploaded_file, directory, file_name=False):
             raise
         location = "{0}/{1}".format(directory, file_name)
 
+    if process_audio:
+        amplified_file_data = get_amplified_file(file_path, 0.0, True, True, None)
+        if amplified_file_data[0]:
+            location = "{0}/{1}".format(directory, amplified_file_data[1].split("/").pop())  # only UNIX safe!
     return location
 
 
@@ -326,3 +331,45 @@ class Paginator():
 
 # paginator
 jquery_dt_paginator = Paginator()
+
+
+def get_normalized_file(input_file, db_level=0):
+    """
+    wrapper for pySox norm command
+    :param input_file: name of the input file to process
+    :param db_level: the db level to which to normalize the audio file
+    :return: Boolean of whether or not the process was successful
+    """
+    try:
+        transformer = sox.Transformer()
+        transformer.norm(db_level)
+        input_file_parts = input_file.split('/')
+        input_file_parts[len(input_file_parts) - 1] = "{0}_{1}_{2}".format("norm", db_level, input_file_parts[len(input_file_parts) - 1])
+        output_file = '/'.join(input_file_parts)
+        transformer.build(input_file, output_file)
+        return True, output_file
+    except:
+        return False, input_file
+
+
+def get_amplified_file(input_file, gain_db=0.0, normalize=True, limiter=True, balance=None):
+    """
+    Wrapper for PySox gain command
+    :param input_file: name of the input file to process
+    :param gain_db: The gain in db to be applied to the file
+    :param normalize: Whether or not to apply normalization
+    :param limiter: Whether or not to use a limiter to prevent clipping
+    :param balance: Balance options. See docs on balance parameter for pysox:gain
+    :return: Boolean of whether or not the process was successful
+    """
+    try:
+        transformer = sox.Transformer()
+        transformer.gain(gain_db, normalize, limiter, balance)
+        input_file_parts = input_file.split('/')
+        input_file_parts[len(input_file_parts) - 1] = "{0}_{1}_{2}".format("gain", gain_db, input_file_parts[len(input_file_parts) - 1])
+        output_file = '/'.join(input_file_parts)
+        transformer.build(input_file, output_file)
+        return True, output_file
+    except Exception as e:
+        print e.message
+        return False, input_file
