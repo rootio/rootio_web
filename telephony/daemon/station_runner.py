3b133c1f1f16b022f7725d9a2fbd18d990fceb0a
Jude Mukundane <jude19love@gmail.com>
2019-06-10 23:59:53 +0100

    individual logging for stations by the station runner service

--- telephony/daemon/station_runner.py
+++ telephony/daemon/station_runner.py
@@ -24,11 +24,10 @@ from ..radio_station import RadioStation
 class StationRunner:
 
     def __init__(self, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
-        self.logger = logging.getLogger('station_runner')
+        self.__logger = self.__get_logger("service")
         self.run()
 
     def run(self):
-        self.__prepare_logging()
         # set up scheduling
         lst_thr = threading.Thread(target=self.__start_listener, args=())
         lst_thr.start()
@@ -38,30 +37,30 @@ class StationRunner:
             session = sessionmaker(bind=engine)()
             stations = session.query(Station).all()
         except Exception as e:
-            self.logger.error(e.message)
+            self.__logger.error(e.message)
             return
 
         for station in stations:
-            radio_station = RadioStation(station, session, self.logger)
-            self.logger.info('launching station : {0}'.format(station.id))
+            logger = self.__get_logger(station.id)
+            radio_station = RadioStation(station, session, logger)
+            logger.info('launching station : {0}'.format(station.id))
             t = threading.Thread(target=radio_station.run, args=())
             t.start()
-        self.logger.info(
+        self.__logger.info(
             '================ Station runner service started at {0} =============='.format(datetime.utcnow()))
 
-    def __prepare_logging(self):
+    def __get_logger(self, station_id):
+        logger = logging.getLogger('station_runner')
         log_folder = DefaultConfig.LOG_FOLDER
-        file_hdlr = TimedRotatingFileHandler(os.path.join(log_folder, 'stations.log'), when='midnight', interval=1)
-        stream_hdlr = StreamHandler(stream=sys.stdout)
+        file_hdlr = TimedRotatingFileHandler(os.path.join(log_folder, 'stations_{0}.log'.format(station_id)), when='midnight', interval=1)
 
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         file_hdlr.setFormatter(formatter)
-        stream_hdlr.setFormatter(formatter)
 
-        self.logger.addHandler(file_hdlr)
-        self.logger.addHandler(stream_hdlr)
+        logger.addHandler(file_hdlr)
 
-        self.logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
+        return logger
 
     def __start_listener(self):
         self.__station_sockets = dict()
@@ -73,17 +72,16 @@ class StationRunner:
                 s.bind((DefaultConfig.SCHEDULE_EVENTS_SERVER_IP, DefaultConfig.SCHEDULE_EVENTS_SERVER_PORT))
                 bound = True
             except:
-                self.logger.warning("Error on server bind, retrying. Retrying in 30 secs...")
+                self.__logger.warning("Error on server bind, retrying. Retrying in 30 secs...")
                 time.sleep(30)
         s.listen(0)
-        self.logger.info("Started TCP listener on port {0}".format(DefaultConfig.SCHEDULE_EVENTS_SERVER_PORT))
+        self.__logger.info("Started TCP listener on port {0}".format(DefaultConfig.SCHEDULE_EVENTS_SERVER_PORT))
 
         while 1:
             cli, adr = s.accept()
             thrd = threading.Thread(target=self.__handle_tcp_connection, args=(cli,))
             thrd.daemon = True
             thrd.start()
-
 
     def __handle_tcp_connection(self, sck):  # TODO: handle json errors, else server will break due to rogue connection
 
@@ -103,24 +101,24 @@ class StationRunner:
             try:
                 event = json.loads(data)
             except ValueError:
-                self.logger.error('JSON load error')
+                self.__logger.error('JSON load error')
                 return
 
         if "action" in event and "station" in event:
             if event["action"] == "register":  # A station is registering
                 self.__station_sockets[event["station"]] = sck
-                self.logger.info("Station {0} has registered for schedule change events".format(event["station"]))
+                self.__logger.info("Station {0} has registered for schedule change events".format(event["station"]))
             elif event["action"] in ["add", "delete", "update", "sync"] and "id" in event:  # Schedule is alerting of a change
                 if event["station"] in self.__station_sockets:
                     try:
                         self.__station_sockets[event["station"]].send(data)
-                        self.logger.info("Event of type {0} sent to station {1} on scheduled program {2}"
+                        self.__logger.info("Event of type {0} sent to station {1} on scheduled program {2}"
                                          .format(event["action"], event["station"], event["id"]))
                     except Exception as e:
-                        self.logger.error("Event of type {0} failed for station {1} on scheduled program {2}"
+                        self.__logger.error("Event of type {0} failed for station {1} on scheduled program {2}"
                                          .format(event["action"], event["station"], event["id"]))
-                        self.logger.error("Event scheduler error: {0}".format(e))
-                        self.logger.error("Broken socket: {0}".format(self.__station_sockets[event['station']]))
+                        self.__logger.error("Event scheduler error: {0}".format(e))
+                        self.__logger.error("Broken socket: {0}".format(self.__station_sockets[event['station']]))
 
 
 if __name__ == "__main__":
