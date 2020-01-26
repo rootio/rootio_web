331be6bd4f4641803264ad15a02e0e8006c8fa63
Jude Mukundane <jude19love@gmail.com>
2020-01-26 13:37:52 +0000

    added translation script to run nightly

--- scripts/update_translations.sh
+++ scripts/update_translations.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#add something like this to crontab file -> crontab -e  in order to run this everyday
+# 0 0 * * * cd /opt/rootio/rootio_web/scripts && bash update_translations.sh
+
 source ../../venvs/rootio_web/bin/activate
 
 cd .. && ./manage.py i18n_update_translations && ./manage.py i18n_compile_translations -f
