346b51bb2787a7f69afd32fe6b09ee5d20b551af
Jude Mukundane <jude19love@gmail.com>
2020-06-29 21:09:17 +0100

    added model for meetings

--- rootio/onair/__init__.py
+++ rootio/onair/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
+from ..radio.models import *
 from .models import *
 from .views import onair
--- rootio/onair/models.py
+++ rootio/onair/models.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Column, Table, types
-from coaster.sqlalchemy import BaseMixin
-from ..extensions import db
-from ..radio import ScheduledProgram
 
+from coaster.sqlalchemy import BaseMixin
+
+from ..extensions import db
+from ..utils import STRING_LEN
+
+t_meetingstation = db.Table(
+    u'governance_meetingstation',
+    db.Column(u'meeting_id', db.ForeignKey('governance_meeting.id')),
+    db.Column(u'station_id', db.ForeignKey('radio_station.id'))
+)
+
+
+class GovernanceTrack(BaseMixin, db.Model):
+    """A track to which audio content is added"""
+    __tablename__ = u'governance_meeting'
+
+    meeting_date = db.Column(db.DateTime(), nullable=False)
+    minutes = db.Column(db.Text(), nullable=False)
+    agenda = db.Column(db.Text(), nullable=False)
+    attendees = db.Column(db.Text(), nullable=False)
+    track_id = db.Column(db.ForeignKey('content_track.id'))
+    creator_id = db.Column(db.ForeignKey('user_user.id'))
+    archived = db.Column(db.Boolean, default=False)
+
+    creator = db.relationship(u'User', backref=db.backref('meetings'))
+    track = db.relationship(u'ContentTrack', backref=db.backref('meetings'))
+    stations = db.relationship(u'Station', secondary=u'governance_meetingstation', backref=db.backref('meetings'))
 
 class OnAirProgram(BaseMixin, db.Model):
     """Repository for actions that occur while the program is on the air.
@@ -28,3 +51,6 @@ class OnAirProgram(BaseMixin, db.Model):
         # init station, program type, etc thru foreign keys
         self.station = self.scheduled_program.station
         self.program_type = self.scheduled_program.program_type
+
+
+
