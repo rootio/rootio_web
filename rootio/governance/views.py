84efc0873ff98887ba13cf3584ced0eb6671f83c
Jude Mukundane <jude19love@gmail.com>
2020-06-30 16:32:25 +0100

    Meeting audio created upon saving and added to track

--- rootio/governance/views.py
+++ rootio/governance/views.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+import os
+import string
+import urllib
 
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask.ext.babel import gettext as _
 from flask.ext.login import login_required, current_user
 
+from rootio.config import DefaultConfig
+from rootio.content import ContentUploads
 from rootio.user import ADMIN
+from telephony.cereproc.cereproc_rest_agent import CereprocRestAgent
 from .forms import GovernanceMeetingForm
 from ..extensions import db, csrf
 from ..onair import GovernanceMeeting
@@ -60,7 +66,17 @@ def edit_meeting(meeting_id):
         form.populate_obj(meeting)
         db.session.add(meeting)
         db.session.commit()
-        flash(_('Meeting updated.'), 'success')
+        if meeting.track is not None:
+            if create_media_for_track(meeting.id, meeting.meeting_date, meeting.stations, meeting.agenda,
+                                      meeting.minutes, meeting.attendees, meeting.track_id):
+                flash(_('Meeting created. An audio transcript for the meeting has been added to the selected track'),
+                      'success')
+            else:
+                flash(_('Meeting created. An audio transcript for the meeting could not be created'), 'warn')
+        else:
+            flash(_('Meeting created.'), 'success')
+    elif request.method == "POST":
+        flash(_('Validation error'), 'error')
     return render_template('governance/governance_meeting.html', meeting=meeting, form=form, active="Meetings")
 
 
@@ -76,9 +92,48 @@ def add_meeting():
         cleaned_data.pop('submit', None)  # remove submit field from list
         meeting = GovernanceMeeting(**cleaned_data)  # create new object from data
         db.session.add(meeting)
+        db.session.flush()
         db.session.commit()
-        flash(_('Meeting created.'), 'success')
+        if meeting.track is not None:
+            if create_media_for_track(meeting.id, meeting.meeting_date, meeting.stations, meeting.agenda, meeting.minutes, meeting.attendees, meeting.track_id):
+                flash(_('Meeting created. An audio transcript for the meeting has been added to the selected track'), 'success')
+            else:
+                flash(_('Meeting created. An audio transcript for the meeting could not be created'), 'warn')
+        else:
+            flash(_('Meeting created.'), 'success')
     elif request.method == "POST":
         flash(_('Validation error'), 'error')
     return render_template('governance/governance_meeting.html', meeting=meeting, form=form, active="Meetings")
 
+
+def create_media_for_track(meeting_id, meeting_date, stations, agenda, minutes, attendees, track_id):
+    # get the text first.
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(my_path, "../templates/governance/meeting_summary_template.txt")
+    body = string.Template(open(file_path).read())
+    details = {"meeting_date": meeting_date, "stations": ",".join(str(stations)), "agenda": agenda, "minutes": minutes, "attendees":attendees}
+    tts_text = body.substitute(details)
+
+    # generate the audio file
+    tts_agent = CereprocRestAgent(DefaultConfig.CEREPROC_SERVER, DefaultConfig.CEREPROC_USERNAME,
+                                             DefaultConfig.CEREPROC_PASSWORD)
+    tts_data = tts_agent.get_cprc_tts(tts_text)
+
+    # download audio file to directory for track
+    try:
+        if not os.path.exists(os.path.join(DefaultConfig.CONTENT_DIR, 'media', str(track_id))):
+            os.makedirs(os.path.join(DefaultConfig.CONTENT_DIR, 'media', str(track_id)))
+        urllib.urlretrieve(tts_data[0],
+                               os.path.join(DefaultConfig.CONTENT_DIR, 'media', str(track_id),  str(meeting_id) + ".mp3"))
+            # create a track file instance
+        content = ContentUploads()
+        content.uri = "{0}/{1}/{2}".format('media', str(track_id), str(meeting_id) + ".mp3")
+        content.track_id = track_id
+        content.uploaded_by = current_user.id
+        content.name = "{0}.mp3".format(meeting_id)
+        db.session.add(content)
+        db.session.flush()
+        db.session.commit()
+        return True
+    except Exception as e:
+        return False
